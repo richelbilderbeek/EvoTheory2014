1f192e4a869df9065c3a68d4ce6558b27b1a2bf4
richelbilderbeek <richel@richelbilderbeek.nl>
2014-12-15 21:48:11 +0100

    Day 1, exercise 4

---
+++ Day1_4_vigilance.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+maxima_input_file="Day1_4_vigilance.txt"
+tex_output_file="Day1_4_vigilance_output.tex"
+
+if [ -e $tex_output_file ]
+then
+  rm $tex_output_file
+fi
+
+maxima -b $maxima_input_file
+pdflatex $tex_output_file
